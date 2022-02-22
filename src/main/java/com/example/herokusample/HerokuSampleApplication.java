4a020ef64fd767f74761bdee01b5ff65a70eed72
godongs <asd236678@gmail.com>
2022-02-22 14:13:16 +0900

    init

---
+++ src/main/java/com/example/herokusample/HerokuSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.herokusample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerokuSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerokuSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/herokusample/HerokuSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.herokusample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerokuSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
