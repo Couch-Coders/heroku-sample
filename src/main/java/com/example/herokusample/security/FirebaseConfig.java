20d36087a0948aa6a7ede026cfcf867d7445b338
고동휘 <asd236678@gmail.com>
2022-04-03 19:46:39 +0900

    add h2 & postgre split properties

--- src/main/java/com/example/herokusample/security/FirebaseConfig.java
+++ src/main/java/com/example/herokusample/security/FirebaseConfig.java
@@ -9,9 +9,11 @@ import com.google.firebase.FirebaseOptions;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 import lombok.extern.slf4j.Slf4j;
 
+@Profile("prod")
 @Configuration
 @Slf4j
 public class FirebaseConfig {
