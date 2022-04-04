ad83788e8d3a0a26e3634e4aac45ecf53a69369c
고동휘 <asd236678@gmail.com>
2022-04-04 19:49:16 +0900

    add construtor

--- src/main/java/com/example/herokusample/security/ProdSecurityConfig.java
+++ src/main/java/com/example/herokusample/security/ProdSecurityConfig.java
@@ -2,16 +2,19 @@ package com.example.herokusample.security;
 
 import com.example.herokusample.security.filter.FirebaseTokenFilter;
 import com.example.herokusample.security.filter.TestTokenFilter;
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 
 @Profile("prod")
 @Configuration
 @EnableWebSecurity
 public class ProdSecurityConfig extends SecurityConfigBase {
-    public ProdSecurityConfig() {
-        super(new FirebaseTokenFilter());
+    public ProdSecurityConfig(UserDetailsService userService, FirebaseAuth firebaseAuth) {
+        super(new FirebaseTokenFilter(userService, firebaseAuth));
     }
 }
--- src/main/java/com/example/herokusample/security/TestSecurityConfig.java
+++ src/main/java/com/example/herokusample/security/TestSecurityConfig.java
@@ -5,12 +5,13 @@ import com.example.herokusample.security.filter.TestTokenFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 
 @Profile("local")
 @Configuration
 @EnableWebSecurity
 public class TestSecurityConfig extends SecurityConfigBase {
-    public TestSecurityConfig() {
-        super(new TestTokenFilter());
+    public TestSecurityConfig(UserDetailsService userDetailsService) {
+        super(new TestTokenFilter(userDetailsService));
     }
 }
