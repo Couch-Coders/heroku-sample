a51ccb5a1b3272ae0714e7bc65560e8517b89d51
고동휘 <asd236678@gmail.com>
2022-04-04 19:35:23 +0900

    add exceptions and filter

---
+++ src/main/java/com/example/herokusample/exception/CustomException.java
@@ -0,0 +1,20 @@
+package com.example.herokusample.exception;
+
+package couch.camping.exception;
+
+import lombok.Getter;
+
+@Getter
+public class CustomException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public CustomException(ErrorCode errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    public CustomException(ErrorCode errorCode, String message ) {
+        super(message);
+        this.errorCode = errorCode;
+    }
+}
